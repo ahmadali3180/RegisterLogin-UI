b0df8c15052cabb0f8bb828441b7bc15116fb0eb
mahmadali3180 <mahmadali3180@gmail.com>
2023-04-13 01:13:55 +0500

    Complete UI

---
+++ RegisterLoginUI/Controllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  RegisterLoginUI
+//
+//  Created by M. Ahmad Ali on 12/04/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
